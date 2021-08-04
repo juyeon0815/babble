fe93f23f7386af722737293d949363d544a8629f
hsh0321 <ppko1233@gmail.com>
2021-08-04 11:05:33 +0900

    fix : 오픈비두 서버 인증서 문제 해결
    letsencrypt로 싸피 인증서 사용하여 해결

--- babble/frontend/src/views/conferences/components/video-space.vue
+++ babble/frontend/src/views/conferences/components/video-space.vue
@@ -56,8 +56,8 @@ import axios from "axios";
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-const OPENVIDU_SERVER_URL = "https://" + "i5a308.p.ssafy.io" + ":4443";
-const OPENVIDU_SERVER_SECRET = "MY_SECRET";
+const OPENVIDU_SERVER_URL = "https://" + "i5a308.p.ssafy.io";
+const OPENVIDU_SERVER_SECRET = "BABBLE";
 
 export default {
   name: "video-space",
