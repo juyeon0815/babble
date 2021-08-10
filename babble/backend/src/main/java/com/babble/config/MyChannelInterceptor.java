9c7b75413cd344a9e243f332ecfceed08d5f5d39
김주현 <j828h@naver.com>
2021-08-10 19:34:40 +0900

    S05P13A308-90 Fix: jwt 검증 제거

--- babble/backend/src/main/java/com/babble/config/MyChannelInterceptor.java
+++ babble/backend/src/main/java/com/babble/config/MyChannelInterceptor.java
@@ -1,9 +1,6 @@
 package com.babble.config;
 
 import com.babble.common.util.JwtTokenUtil;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,14 +30,14 @@ public class MyChannelInterceptor implements ChannelInterceptor {
             System.out.println(">>> accessor " + accessor);
             String jwt = accessor.getFirstNativeHeader("authorization");
             System.out.println("jwt " + jwt);
-            if(jwt!=null) {
-                // 토큰 유효성 검사를 해서, 토큰이 있으면 Member, 없으면 Guest Role 부여
-                // 전달받은 토큰이 제대로 생성된 것인지 확인
-                boolean valid = JwtTokenUtil.validateToken(jwt);
-                System.out.println(">>> 사용 가능한 토큰??? " + valid);
-                System.out.println(JwtTokenUtil.getUserInfo(jwt));
-                // 토큰이 유효하지 않으면 예외 처리 되어서 메시지 수신 되지 않는다.
-            }
+//            if(jwt!=null) {
+//                // 토큰 유효성 검사를 해서, 토큰이 있으면 Member, 없으면 Guest Role 부여
+//                // 전달받은 토큰이 제대로 생성된 것인지 확인
+//                boolean valid = JwtTokenUtil.validateToken(jwt);
+//                System.out.println(">>> 사용 가능한 토큰??? " + valid);
+//                System.out.println(JwtTokenUtil.getUserInfo(jwt));
+//                // 토큰이 유효하지 않으면 예외 처리 되어서 메시지 수신 되지 않는다.
+//            }
         } else if (command.compareTo(StompCommand.DISCONNECT) == 0) {
             System.out.println("사용자 연결 해제");
         }
